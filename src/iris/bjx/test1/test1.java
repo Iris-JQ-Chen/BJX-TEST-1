e086838fe44a1a4ddee0dc88749971705fdb810b
Iris-JQ-Chen <2906514421@qq.com>
2021-03-16 10:59:20 +0800

    initial jdbc connecter

--- src/iris/bjx/test1/test1.java
+++ src/iris/bjx/test1/test1.java
@@ -1,8 +1,52 @@
 package iris.bjx.test1;
 
+import java.sql.*;
+
 public class test1 {
 
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/BJX0316";
+
+    static final String USER = "root";
+    static final String PASS = "cjqloveldf999";
+
     public static void main(String[] args){
+
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL,USER,PASS);
+            statement = connection.createStatement();
+            String sql;
+            sql = "SELECT id, sentence FROM BJX0316.test";
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String sentence = resultSet.getString("sentence");
+                System.out.print("ID: "+id+"\t");
+                System.out.println("SENTENCE:"+sentence);
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            // 关闭资源
+            try{
+                if(statement!=null) statement.close();
+            }catch(SQLException se2){
+            }// 什么都不做
+            try{
+                if(connection!=null) connection.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
 
     }
 
